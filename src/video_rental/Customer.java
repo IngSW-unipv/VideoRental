18cc61ce3cec1cc79fd439a5b10c76a6f3b4d7c4
Claudio Cusano <claudio.cusano@unipv.it>
2020-06-03 11:10:49 +0200

    Extract two methods

--- src/video_rental/Customer.java
+++ src/video_rental/Customer.java
@@ -27,27 +27,9 @@ public class Customer {
 		for (Rental each : _rentals) {
 			double thisAmount = 0;
 			//determine amounts for each line
-			switch (each.getMovie().getPriceCode()) {
-			case Movie.REGULAR:
-				thisAmount += 2;
-				if (each.getDaysRented() > 2)
-					thisAmount += (each.getDaysRented() - 2) * 1.5;
-				break;
-			case Movie.NEW_RELEASE:
-				thisAmount += each.getDaysRented() * 3;
-				break;
-			case Movie.CHILDREN:
-				thisAmount += 1.5;
-				if (each.getDaysRented() > 3)
-					thisAmount += (each.getDaysRented() - 3) * 1.5;
-				break;
-			}
-
+			thisAmount = each.getCharge();
 			// add frequent renter points
-			frequentRenterPoints ++;
-			// add bonus for a two day new release rental
-			if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-					each.getDaysRented() > 1) frequentRenterPoints++;
+			frequentRenterPoints += each.getFreqeuntRenterPoints();
 			// show figures for this rental
 			result += "\t" + each.getMovie().getTitle()+ "\t" +
 					String.valueOf(thisAmount) + "\n";
@@ -59,4 +41,6 @@ public class Customer {
 		+ " frequent renter points";
 		return result;
 	}
+
+	
 }
