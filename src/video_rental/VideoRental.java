30037b3098d15c1a78eaee905c4fa64dd7e32b99
Claudio Cusano <claudio.cusano@unipv.it>
2020-06-03 10:54:16 +0200

    Initial source files

---
+++ src/video_rental/VideoRental.java
@@ -0,0 +1,11 @@
+package video_rental;
+
+public class VideoRental {
+
+	public static void main(String[] args) {
+		Movie movie1 = new Movie("Shining", Movie.REGULAR);
+		Movie movie2 = new Movie("Fnding Nemo", Movie.CHILDREN);
+		Movie movie3 = new Movie("Star Wars XXIII", Movie.NEW_RELEASE);
+	}
+
+}
